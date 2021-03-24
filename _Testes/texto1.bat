74b63aec47cdbc4b3c0d12a871520af76654df95
Joca <aspirantevitor@gmail.com>
2021-03-24 19:58:18 -0300

    Commitei meu primeiro arquivo no Git

---
+++ _Testes/texto1.bat
@@ -0,0 +1,9 @@
+::Organização de pastas através do Cmder
+clear
+@echo off
+echo Vamos trabalhar com o Shell Script?
+echo 'code1'
+echo 'code2'
+echo 'bash shell pythonpy'
+mensagem="Vamos aprender a dominar o terminal!"
+echo $mensagem
