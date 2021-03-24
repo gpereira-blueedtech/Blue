311ee7d3e5a95d3e4ea084d05e0dde00bd2aeb3c
Joca <aspirantevitor@gmail.com>
2021-03-24 20:02:16 -0300

    Fiz minha primeira alteração no arquivo texto1.bat

--- _Testes/texto1.bat
+++ _Testes/texto1.bat
@@ -5,5 +5,4 @@ echo Vamos trabalhar com o Shell Script?
 echo 'code1'
 echo 'code2'
 echo 'bash shell pythonpy'
-mensagem="Vamos aprender a dominar o terminal!"
-echo $mensagem
+echo Vamos aprender a usar o Git na aula 18
