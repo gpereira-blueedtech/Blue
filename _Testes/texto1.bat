c79cdf01332695636f35ebf6f3c369643a4389f4
Joca <aspirantevitor@gmail.com>
2021-03-24 20:18:28 -0300

    Texto1 | Versão 1.1

--- _Testes/texto1.bat
+++ _Testes/texto1.bat
@@ -2,6 +2,12 @@
 @echo off
 echo Vamos trabalhar com o Shell Script?
 echo 'code1'
-echo 'code2'
+@echo 'code2'
 echo 'bash shell pythonpy'
 echo Vamos aprender a usar o Git na aula 18
+
+echo PYTHON SCRIPT #001
+
+nome = input('Informe seu nome: ')
+
+print(f'Bem vindo ao Git, {nome}!')
